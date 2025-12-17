db4d4a0de49447a3da01be509128f5ea4b33b489
Dario George <edu.dariogeorge21@gmail.com>
2025-12-17 13:46:29 +0530

    Remove Schedule link from Navigation component

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -20,7 +20,7 @@ export default function Navigation() {
   const navLinks = [
     { name: 'Home', href: '#home' },
     { name: 'About', href: '#about' },
-    { name: 'Schedule', href: '#schedule' },
+    // { name: 'Schedule', href: '#schedule' },
     { name: 'Competitions', href: '#competitions' },
     { name: 'Contact', href: '#contact' },
   ];
